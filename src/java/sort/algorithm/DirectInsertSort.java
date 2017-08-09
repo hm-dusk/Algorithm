4c950db039d338555f85fba29de51063b0a8808a
dusk <cqupt_hm@163.com>
2017-08-09 21:33:03 +0800

    1

--- src/java/sort/algorithm/DirectInsertSort.java
+++ src/java/sort/algorithm/DirectInsertSort.java
@@ -2,8 +2,13 @@ package sort.algorithm;
 
 /**
  * 直接插入排序
- * <p>
+ *
  * Created by liming on 2017/8/9
  */
 public class DirectInsertSort {
+	private int a[] = {49, 38, 65, 97, 76, 13, 27, 49, 78, 34, 12, 64, 5, 4, 62, 99, 98, 54, 56, 17, 18, 23, 34, 15, 35, 25, 53, 51};
+
+	public DirectInsertSort() {
+		int temp = 0;
+	}
 }
